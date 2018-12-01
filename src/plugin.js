9c3c1900ab6c48cf1038984dd7a70b6f92a37614
fatfanfan <fatfan1020@gmail.com>
2018-12-01 22:18:04 +0800

    解决一个 currentToast 的一个bug

--- src/plugin.js
+++ src/plugin.js
@@ -9,17 +9,26 @@ let currentToast
        if (currentToast) {
          currentToast.close()
        }
-       currentToast = createToast({Vue, message, propsData: toastOptions})
+       currentToast = createToast({
+         Vue,
+         message,
+         propsData: toastOptions,
+         onClose: () => {
+           console.log('closed');
+           currentToast = null
+         }
+       })
      }
    }
  }
 
 /* helpers */
-function createToast ({Vue, message, propsData}) {
+function createToast ({Vue, message, propsData,onClose}) {
   let Constructor = Vue.extend(Toast)
   let toast = new Constructor({propsData})
   toast.$slots.default = [message]
   toast.$mount()
+  toast.$on('close', onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
