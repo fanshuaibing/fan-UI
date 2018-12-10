df09ebbd1b7cf01ad91b7e9c2a1fbd6c53016284
fatfanfan <fatfan1020@gmail.com>
2018-12-10 22:29:15 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
+    <div class="title" @click="toggle">
       {{title}}
     </div>
     <div class="content" v-if="open">
@@ -18,11 +18,33 @@
         required: true
       }
     },
+    inject:['eventBus'],
     data() {
       return {
         open: false
       }
+    },
+    mounted(){
+      this.eventBus && this.eventBus.$on('update:selected',(vm)=>{
+        if(vm !==this){
+          this.close()
+        }
+      })
+    },
+    methods:{
+      toggle() {
+        if (this.open) {
+          this.open = false
+        }else {
+          this.open = true
+          this.eventBus && this.eventBus.$emit('update:selected',this)
+        }
+      },
+      close(){
+        this.open = false
+      }
     }
+
   }
 </script>
 
