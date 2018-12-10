8e15cf38533c6409ec240f9ea3d3a6249f27fb5c
fatfanfan <fatfan1020@gmail.com>
2018-12-10 14:27:54 +0800

    完成基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" >
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -17,23 +17,37 @@
       return {visible: false}
     },
     methods: {
-      xxx() {
-        this.visible = !this.visible;
-        if (this.visible === true) {
-          this.$nextTick(()=>{
-            document.body.appendChild(this.$refs.contentWrapper);
-            let {width, height ,top , left } = this.$refs.triggerWrapper.getBoundingClientRect();
-            this.$refs.contentWrapper.style.left = left + window.scrollX +'px';
-            this.$refs.contentWrapper.style.top = top + window.scrollY +'px';
-            let eventHandler =()=>{
-              this.visible = false;
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper);
+        let {width, height ,top , left } = this.$refs.triggerWrapper.getBoundingClientRect();
+        this.$refs.contentWrapper.style.left = left + window.scrollX +'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY +'px';
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+            (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open(){
+        this.visible= true;
+        this.$nextTick(()=>{
+          this.positionContent();
+          document.addEventListener('click',this.onClickDocument)
+        })
 
-              document.removeEventListener('click',eventHandler)
-            };
-            document.removeEventListener('click',eventHandler)
-          })
-        }else {
-          console.log('vm close popover')
+      },
+      close(){
+        this.visible = false;
+        document.removeEventListener('click',this.onClickDocument)
+      },
+      onClick(event){
+        if(this.$refs.triggerWrapper.contains(event.target)){
+          if(this.visible=== true){
+            this.close()
+          }else{
+            this.open()
+          }
         }
       }
     }
