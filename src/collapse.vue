9deeec21d805ac3de7f043cd4d16961f6ff45b52
fatfanfan <fatfan1020@gmail.com>
2018-12-10 22:15:45 +0800

    collapse 基本样式和基本功能

---
+++ src/collapse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "fanCollapse"
+  }
+</script>
+
+<style scoped lang="scss">
+  $gray:#ddd;
+  $border-radius:4px;
+  .collapse{
+    border: 1px solid $gray;
+    border-radius: $border-radius;
+  }
+</style>
