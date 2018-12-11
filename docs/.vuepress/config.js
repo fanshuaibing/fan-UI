7d6133cec7a83b221f825a781db82f11ce884e9a
fatfanfan <fatfan1020@gmail.com>
2018-12-11 20:51:47 +0800

    实现官网雏形

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: '凡人 UI 官网',
+  description: '一个好用的 UI 框架',
+  themeConfig:{
+    sidebar: [
+      {
+        title:'入门',
+        children:[
+           '/install/',
+            '/get-started/'
+        ]
+      },
+      {
+        title:'组件',
+        children:[
+            '/components/button.md',
+            '/components/input.md',
+            '/components/grid.md',
+            '/components/layout.md',
+            '/components/toast.md',
+            '/components/tab.md',
+            '/components/popover.md',
+            '/components/collapse.md',
+
+        ]
+      }
+    ]
+  }
+}
---
+++ docs/components/button.md
@@ -0,0 +1,21 @@
+---
+title: Button 
+---
+# 按钮
+
+## 使用方法
+
+### 预览
+---
+<button-demos></button-demos>
+
+### 代码
+```
+<g-button>默认按钮</g-button>
+<g-button icon="settings">默认按钮</g-button>
+<g-button :loading="true">默认按钮</g-button>
+<g-button disabled>默认按钮</g-button>
+
+```
+
+
